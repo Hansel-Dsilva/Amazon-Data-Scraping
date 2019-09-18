551e67939affa815ac2e768328ef20aebeee8f52
Hansel D'silva <hanseldsilvag@gmail.com>
2019-09-18 20:16:28 +0530

    test1

--- myCode/code.py
+++ myCode/code.py
@@ -1,12 +1,13 @@
 # base_url = 'https://www.amazon.in/s?k=men+tshirts&page=2&qid=1568399887&ref=sr_pg_' + str(page)
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 
 def scrape():
-    l = []
-    for page in range(3):
-        page = page + 1
+    productData = pd.DataFrame()
+    for page in range(1,4):
+
         base_url = 'https://www.amazon.in/s?k=men+tshirts&page=2&qid=1568399887&ref=sr_pg_' + str(page)
         # print(base_url)
 
@@ -15,46 +16,18 @@ def scrape():
         soup = BeautifulSoup(r.text, "html.parser")
 
         #all_product = soup.find_all(lambda tag: tag.name == 'span')
+        class_ = ["a-size-base-plus a-color-base a-text-normal", "a-price-whole", "a-size-base-plus a-color-base"]
+        all_product = soup.find_all('span', class_)
+        #print(len(all_product))
 
-        all_product = soup.find_all('span', class_="a-size-base-plus a-color-base a-text-normal")
-        print(len(all_product))
-
+        counter = 1
         for item in all_product:
             d = {}
 
-            # image
-            product_image = item.find("img", {"class":"product-media__img"})
-            # image = image.text.replace('\n', "").strip()
-            product_image = product_image['src']
-            d['product_image'] = product_image
+        d['Brand'] = item.string
 
-            # name & link
-            product_name = item.find("a", {"class":"product__name"})
-            product_link = 'https://www.bukalapak.com' + str(product_name.get('href'))
-            product_name = product_name.text.replace('\n', "").strip()
-            d['product_link'] = product_link
-            d['product_name'] = product_name
 
-            # price
-            product_price = item.find("span", {"class":"amount"})
-            product_price = product_price.text.replace('\n', "").strip()
-            d['product_price'] = 'Rp' + product_price
-
-            # review
-            product_review = item.find("a", {"class":"review__aggregate"})
-            try:
-                product_review = product_review.text
-                d['product_review'] = int(product_review)
-            except:
-                d['product_review'] = 0
-
-            # link
-            # product_link = item.find("a", {"class":"product-media__link"}, href=True)
-            # product_link = 'https://www.bukalapak.com' + str(product_link.get('href'))
-            # d['product_link'] = product_link
-
-            l.append(d)
-
+        l.append(d)
     return l
 
 
