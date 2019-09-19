71f767b85863fe61c7e83b8ff49076185932f031
Hansel D'silva <hanseldsilvag@gmail.com>
2019-09-19 17:22:41 +0530

    test2

--- myCode/code.py
+++ myCode/code.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 
 def scrape():
-    productData = pd.DataFrame()
+    productData = pd.DataFrame(columns=['Product_name', 'Price', 'Date', 'Time'])
     for page in range(1,4):
 
-        base_url = 'https://www.amazon.in/s?k=men+tshirts&page=2&qid=1568399887&ref=sr_pg_' + str(page)
+        base_url = 'https://www.amazon.in/s?k=tommy+hilfiger+t+shirt+for+men&page=' + str(page)
         # print(base_url)
 
         # Request URL and Beautiful Parser
@@ -16,19 +16,22 @@ def scrape():
         soup = BeautifulSoup(r.text, "html.parser")
 
         #all_product = soup.find_all(lambda tag: tag.name == 'span')
-        class_ = ["a-size-base-plus a-color-base a-text-normal", "a-price-whole", "a-size-base-plus a-color-base"]
+        class_ = ["a-size-base-plus a-color-base a-text-normal", "a-price-whole"]
         all_product = soup.find_all('span', class_)
-        #print(len(all_product))
+        print(len(all_product))
 
         counter = 1
         for item in all_product:
-            d = {}
+            if counter == 1:
 
-        d['Brand'] = item.string
+            if counter == 2:
+                counter = 1
 
+            #productData = productData.append(str(item.string))
 
-        l.append(d)
-    return l
+            counter += 1
+        #productData.append(d)
+    return productData
 
 
 if __name__ == "__main__":
